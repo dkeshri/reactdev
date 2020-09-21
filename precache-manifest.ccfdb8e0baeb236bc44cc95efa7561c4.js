c7c342e059f5f1271e93a5e8745167a7c6578ad6
Deepak Keshri <dkeshridev@gmail.com>
2020-09-21 12:41:25 +0530

    Updates

---
+++ precache-manifest.ccfdb8e0baeb236bc44cc95efa7561c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83a7ee326fb19e0d285c2320eee2117",
+    "url": "/reactdev/index.html"
+  },
+  {
+    "revision": "5a1ce87bb6ebde832f26",
+    "url": "/reactdev/static/css/main.a17641d7.chunk.css"
+  },
+  {
+    "revision": "9ef0ad2906a2edb93b8d",
+    "url": "/reactdev/static/js/2.1e39241b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reactdev/static/js/2.1e39241b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1ce87bb6ebde832f26",
+    "url": "/reactdev/static/js/main.fbfaee8d.chunk.js"
+  },
+  {
+    "revision": "7b75be7503a6bcb456ef",
+    "url": "/reactdev/static/js/runtime-main.5569c41c.js"
+  }
+]);
